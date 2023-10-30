017f684a019c5af59bcd28aad0b896f5169be244
Hieu Tran <hieutran2@nashtechglobal.com>
2023-10-30 17:04:03 +0700

    Install image updater

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -3,7 +3,7 @@ resource "helm_release" "argocd" {
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   namespace        = "argocd"
-  version          = "4.9.7"
+  version          = "5.48.0"
   create_namespace = true
   values = [
     file("../../infrastructure/argocd-template/argocd-install.yaml")
